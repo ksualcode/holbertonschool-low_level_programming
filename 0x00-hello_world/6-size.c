9e1a9c74a8c86322b16890d7ec651e240b1c980a
Daniel Millan <2758@holbertonschool.com>
2021-02-03 16:26:09 +0000

    fixing empty spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,11 @@ int main(void)
 	long long ll;
 	float f;
 
-	printf ("Size of a char: %zu byte(s)\n", sizeof(c));
-	printf ("Size of an int: %zu byte(s)\n", sizeof(i));
-	printf ("Size of a long int: %zu byte(s)\n", sizeof(l));
-	printf ("Size of a long long int: %zu byte(s)\n", sizeof(ll));
-	printf ("Size of a float: %zu byte(s)\n", sizeof(f));
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
+
 	return (0);
 }

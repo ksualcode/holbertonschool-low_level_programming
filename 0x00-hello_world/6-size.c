4a95c62dd366220e0c91c0d4dff2eddd0ac6daac
Daniel Millan <2758@holbertonschool.com>
2021-02-03 16:11:15 +0000

    Adding 5 and 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
+
+	printf ("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf ("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf ("Size of a long int: %zu byte(s)\n", sizeof(l));
+	printf ("Size of a long long: %zu byte(s)\n", sizeof(ll));
+	printf ("Size of a float: %zu byte(s)\n", sizeof(f));
+
+	return (0);
+}

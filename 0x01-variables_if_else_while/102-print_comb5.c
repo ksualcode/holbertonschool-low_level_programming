ac2a43d60e55ba096b4cc7e167240864ac59237e
Ksualboy <77964799+Ksualboy@users.noreply.github.com>
2021-02-04 20:59:18 -0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,38 +3,40 @@
 /**
  * main - Entry point
  *
+ * Description: Show a message blablabla
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j, k, l;
+	int d1, d2, u1, u2, i;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			for (k = i; k < 10; k++)
+	for (d1 = '0'; d1 <= '9'; d1++)
+		for (u1 = '0'; u1 <= '9'; u1++)
+			for (d2 = d1; d2 <= '9'; d2++)
 			{
-				if (i == k)
-					l = j + 1;
+				if (d1 == d2)
+					i = u1 + 1;
 				else
-					l = '0';
-				for (l = l; l < 10; l++)
+					i = '0';
+				for (u2 = i; u2 <= '9'; u2++)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
+					putchar(d1);
+					putchar(u1);
 					putchar(' ');
-					putchar(k + '0');
-					putchar(l + '0');
-					if (i == 9 && j == 8 && k == 9 && l == 9)
+					putchar(d2);
+					putchar(u2);
+					if (d1 == '9' && u1 == '8' && d1 == '9' && u2 == '9')
+					{
 						break;
-					putchar(',');
-					putchar(' ');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-			}
 		}
-	}
 	putchar('\n');
 	return (0);
 }

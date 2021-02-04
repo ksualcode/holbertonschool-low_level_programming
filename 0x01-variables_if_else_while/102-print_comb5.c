f4e2938173df63bec3406ea90363d4bd134c8fef
Daniel Millan <2758@holbertonschool.com>
2021-02-04 20:43:59 +0000

    saving broke 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int i, j, k, l;
+
+        for (i = 0; i < 8; i++)
+        {
+                for (j = 0; j < 9; j++)
+                {
+                        for (k = i; k < 10; k++)
+                        {
+				if (i == k)
+					l = j + 1;
+				else
+					l = 0;
+				for (l; l < 10; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (i == 9 && j == 8)
+						break;
+					putchar(',');
+					putchar(' ');
+                        	}
+			}
+                }
+        }
+        putchar('\n');
+        return (0);
+}

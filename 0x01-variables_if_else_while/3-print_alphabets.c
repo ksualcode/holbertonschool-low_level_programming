461187f09fad28036404eb1cc583324af0f5fee0
Daniel Millan <2758@holbertonschool.com>
2021-02-04 15:47:13 +0000

    Adding 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,12 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c > 'Z'; c++)
-	{
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-		if (c == 'z')
-			c = 'A';
 
-	}
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
 	putchar('\n');
 	return (0);
 

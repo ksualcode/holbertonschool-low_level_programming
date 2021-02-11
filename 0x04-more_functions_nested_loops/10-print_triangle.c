0a24a3453ad48245cc3f03290a62fc3217c5cad6
Daniel Millan <2758@holbertonschool.com>
2021-02-11 18:10:07 +0000

    fixing spaces

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,18 +9,18 @@
 
 void print_triangle(int size)
 {
-        int i, j;
+	int i, j;
 
-        if (size > 0)
-                for (i = size; i > 0; i--)
-                {
-                        for (j = 1; j <= size; j++)
+	if (size > 0)
+		for (i = size; i > 0; i--)
+		{
+			for (j = 1; j <= size; j++)
 				if (j >= i)
-                                	_putchar('#');
+					_putchar('#');
 				else
 					_putchar(' ');
-                        _putchar('\n');
-                }
-        else
-                _putchar('\n');
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
 }

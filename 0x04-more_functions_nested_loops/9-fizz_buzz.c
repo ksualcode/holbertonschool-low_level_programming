3e79bbef40e30c9587d9b9fd9c7bd585ecfcfc52
Daniel Millan <2758@holbertonschool.com>
2021-02-11 16:58:38 +0000

    adding 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 && i % 5)
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}

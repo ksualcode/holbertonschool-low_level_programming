9dcf4362b66895ba9dbd6e6662fd51230073cb0f
Daniel Millan <2758@holbertonschool.com>
2021-02-20 00:40:23 +0000

    adding 100

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints a number
+ * @n: int to print
+ *
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n / 10 != 0)
+	{
+		m = n / 10;
+		print_number(m);
+	}
+	_putchar(n % 10 + '0');
+}

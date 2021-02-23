10825482a0c78e6e9126eb3e2ed91802c47fc469
Daniel Millan <2758@holbertonschool.com>
2021-02-23 17:00:02 +0000

    adding 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,6 +3,7 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string to process
+ * @accept: accepted chars
  *
  * Return: a number of bytes
  */
@@ -10,13 +11,25 @@
 unsigned int _strspn(char *s, char *accept)
 {
 
-	int i, j, count;
+	int i, j;
+	int count = 0;
+	int bool = 0;
 
-	for (i = 0; accept[i]; i++)
-		for (j = 0; s[j]; j++)
+	for (i = 0; s[i]; i++)
+	{
+		bool = 0;
+		for (j = 0; accept[j]; j++)
 		{
-			if 
-
+			if (s[i] == accept[j])
+			{
+				bool = 1;
+				count++;
+			}
 		}
+		if (!bool)
+			break;
+	}
+
+	return (count);
 
 }

0b3493c49e553c3b1a42dc06550727bc012377d6
Daniel Millan <2758@holbertonschool.com>
2021-02-23 22:53:42 +0000

    adding 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,10 +12,13 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	/* int count = 0; */
+	int count = 0;
 
 	for (i = 0; i < size; i++)
-	{
-		printf("%d\n", (*(a + i) + i));
-	}
+		count += *(a + (i * size + i));
+	printf("%d, ", count);
+	count = 0;
+	for (i = 1; i <= size; i++)
+		count += *(a + (i * size - i));
+	printf("%d\n", count);
 }

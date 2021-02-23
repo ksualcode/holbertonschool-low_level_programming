4977e6b54eb86b039a88a99a48644ab28bc97cb6
Daniel Millan <2758@holbertonschool.com>
2021-02-23 23:10:31 +0000

    adding 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @to: string to copy
+ * @s: string to modify
+ *
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

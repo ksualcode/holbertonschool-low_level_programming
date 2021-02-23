26fa213cfa76f1e92592489a4219c70d8e717db6
Daniel Millan <2758@holbertonschool.com>
2021-02-23 23:20:30 +0000

    optimizing if condition

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,28 +12,18 @@ char *_strstr(char *haystack, char *needle)
 {
 
 	int i, j;
-	int bool = 0;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i + j] == needle[j])
-			{
-				/* _putchar('Y'); */
-				bool = 1;
-			}
-			else
-			{
-				/* _putchar('N'); */
-				bool = 0;
+			if (haystack[i + j] != needle[j])
 				break;
-			}
 		}
-		if (bool)
+		if (!needle[j])
 			return ((haystack + i));
 	}
-	/* _putchar('E'); */
+
 	return (0);
 
 }

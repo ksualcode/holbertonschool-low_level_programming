e2460112f5aaa82bb09fc7f838d96251f4690ada
Daniel Millan <2758@holbertonschool.com>
2021-02-24 17:27:20 +0000

    adding 4

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: the number to root
+ *
+ * Return: the root of a numer
+ */
+
+int _sqrt_recursion(int n)
+{
+	
+}

ae4d43bec3c33641cf2931b1d23114ea97f0e954
Daniel Millan <2758@holbertonschool.com>
2021-02-24 18:02:25 +0000

    Adding 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,27 @@
 #include "holberton.h"
 
 /**
+ * get_square - returns the natural square root of a number.
+ * @n: the number to calculate
+ * @i: integer to iterate
+ *
+ * Return: the root of a numer
+ */
+
+int get_square (int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i == n)
+		return (-1);
+	else if (i * i != n)
+		return (get_sqrt(n, i + 1));
+
+	return (0);
+}
+
+
+/**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: the number to root
  *
@@ -9,5 +30,7 @@
 
 int _sqrt_recursion(int n)
 {
-	
+	if (n < 0)
+		return (-1);
+	return (get_sqrt(n, 1));
 }

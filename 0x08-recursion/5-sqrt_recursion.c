848a35fbd6080d349c31ebf53d5f154c7258ccff
Ksualboy <77964799+Ksualboy@users.noreply.github.com>
2021-02-24 15:04:50 -0300

    fixing function name

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,14 +8,14 @@
  * Return: the root of a numer
  */
 
-int get_square (int n, int i)
+int get_square(int n, int i)
 {
 	if (i * i == n)
 		return (i);
 	else if (i == n)
 		return (-1);
 	else if (i * i != n)
-		return (get_sqrt(n, i + 1));
+		return (get_square(n, i + 1));
 
 	return (0);
 }
@@ -32,5 +32,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (get_sqrt(n, 1));
+	return (get_square(n, 1));
 }

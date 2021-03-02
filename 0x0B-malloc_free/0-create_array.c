6af7bf059d33dc67a05d41ef4066f12739e9cf4b
Daniel Millan <2758@holbertonschool.com>
2021-03-02 16:10:28 +0000

    adding 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Creates an array of size characters
+ * @size: Size of the array
+ * @c: Character to initialize
+ *
+ * Return: NULL if fails, pointer if it works
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *uwu;
+	unsigned int i;
+
+	if (size <= 0)
+		return ('\0');
+
+	uwu = malloc(sizeof(char) * size);
+	if (uwu == '\0')
+		return ('\0');
+
+	for (i = 0; i < size; i++)
+		uwu[i] = c;
+
+	return (uwu);
+}

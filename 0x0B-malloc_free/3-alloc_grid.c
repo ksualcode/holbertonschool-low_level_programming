354d2230dcc6c469f952fc6e0bbbefc19b076657
Daniel Millan <2758@holbertonschool.com>
2021-03-02 19:39:13 +0000

    Adding a nasty 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: a double pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **uwu, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	uwu = malloc(sizeof(int) * height);
+	if (uwu == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		uwu[i] = malloc(sizeof(int) * width);
+		if (uwu[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+				free(uwu[i]);
+			free(uwu);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			uwu[i][j] = 0;
+	return (uwu);
+}

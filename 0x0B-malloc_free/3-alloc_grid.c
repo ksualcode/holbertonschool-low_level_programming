e34dcc36c47abc4a028350d6172c8096d31caf20
Daniel Millan <2758@holbertonschool.com>
2021-03-02 19:46:16 +0000

    fixing *

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	uwu = malloc(sizeof(int) * height);
+	uwu = malloc(sizeof(int *) * height);
 	if (uwu == NULL)
 		return (NULL);
 

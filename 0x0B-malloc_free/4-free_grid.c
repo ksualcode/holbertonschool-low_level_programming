c73ae453e7855a71c100e19e7a4175cfb8cc6d89
Daniel Millan <2758@holbertonschool.com>
2021-03-02 20:10:21 +0000

    adding 4 incomplete

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created
+ * @grid: grid to clean
+ * @height: height of the string
+ *
+ * Return: nothing
+ */
+
+
+void free_grid(int **grid, int height)
+{
+	
+}

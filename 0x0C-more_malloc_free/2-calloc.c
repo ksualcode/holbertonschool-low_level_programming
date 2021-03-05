87047be7bba19415ca09e0105fc09b8ffafb6b0a
Daniel Millan <2758@holbertonschool.com>
2021-03-05 01:13:17 +0000

    adding 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * hecked - allocates memory using malloc
+ * @b: amount of memory to allocate
+ *
+ * Return: nothing
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *uwu;
+	unsigned int i;
+
+	if (nmemb == 0 ; size == 0)
+		return (NULL);
+
+	uwu = malloc(size * nmemb);
+	if (!uwu)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		uwu[i] = 0;
+
+	return (uwu);
+}

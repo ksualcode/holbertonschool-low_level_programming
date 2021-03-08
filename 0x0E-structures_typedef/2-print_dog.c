5563a8a592cb4252207787107a7d91483573ca18
Daniel Millan <2758@holbertonschool.com>
2021-03-08 17:28:01 +0000

    Fixing if name or owner is null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,8 +12,16 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf ("Name: %s\n", d->name);
-		printf ("Age: %f\n", d->age);
-		printf ("Owner: %s\n", d->owner);
+		if (d->name != 0)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)");
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != 0)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)");
 	}
 }

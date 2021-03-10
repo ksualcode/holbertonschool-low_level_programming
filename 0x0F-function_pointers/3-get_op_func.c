79875f46b2c6a8dc9660f6408b5865e878b016dd
Daniel Millan <2758@holbertonschool.com>
2021-03-10 16:20:12 +0000

    adding incomplete 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: char to check
+ *
+ * Return: the result of the compute
+ */
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op == s)
+			return (ops[i].f(a, b));
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two variables
+ * @a: 1st number
+ * @b: 2nd number
+ *
+ * Return: the result of the compute
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Substracts two variables
+ * @a: 1st number
+ * @b: 2nd number
+ *
+ * Return: the result of the compute
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two variables
+ * @a: 1st number
+ * @b: 2nd number
+ *
+ * Return: the result of the compute
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two variables
+ * @a: 1st number
+ * @b: 2nd number
+ *
+ * Return: the result of the compute
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Gets the module of two variables
+ * @a: 1st number
+ * @b: 2nd number
+ *
+ * Return: the result of the compute
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

e22e43ad274bc61333089784e961464b8575d5f4
Daniel Millan <2758@holbertonschool.com>
2021-03-11 18:45:52 +0000

    adding README, 0 and header file

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Adds all the parameters given
+ * @argc: amount of arguments passed
+ * @argv: argmuents passed
+ *
+ * Return: 0 if succesfull
+ */
+
+ int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int num = 0;
+	va_list list;
+
+	if (n <= 0)
+		return (0);
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		num += va_arg(list, int);
+	}
+
+	return (num);
+}

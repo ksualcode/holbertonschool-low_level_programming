c0d4c20b3c9c7ab60ae9cbc8b878570ebdcf102d
ksualboy <danielmillanalvarez@gmail.com>
2021-03-18 19:51:12 +0000

    adding 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+* _strlen - counts the ammount of characters of a string
+* @str: string to count
+*
+* Return: The ammount of characters
+*/
+
+unsigned int _strlen(char *str)
+{
+	unsigned int i = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
+* add_node_end - adds a node to the beginning of a list
+* @head: list to base in
+* @str: string to copy
+*
+* Return: The direction of the new address
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp;
+	list_t *search = *head;
+
+	if (!head)
+		return (NULL);
+
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	if (!temp->str)
+	{
+		free(temp);
+		return (NULL);
+	}
+
+	temp->len = _strlen((char *)str);
+	temp->next = NULL;
+
+	if (!search)
+		*head = temp;
+	else
+	{
+		while (search->next)
+			search = search->next;
+
+		search->next = temp;
+	}
+
+	return (temp);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -26,6 +26,7 @@ typedef struct list_s
 
 /* ---- Prototypes ---- */
 int _putchar(char);
+unsigned int _strlen(char *);
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);

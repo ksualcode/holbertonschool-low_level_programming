430cbd33c65093602e2f550222b1f84259c6b75a
ksualboy <danielmillanalvarez@gmail.com>
2021-03-22 14:06:26 +0000

    Adding 4 and 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* free_listint2 - frees a list
+* @head: list to free
+*
+*/
+
+void free_listint2(listint_t *head)
+{
+	listint_t *buscador = head;
+	listint_t *temp;
+
+	if (!head)
+		return;
+
+	while (buscador)
+	{
+		temp = buscador;
+		buscador = buscador->next;
+		free(temp);
+	}
+	head = NULL;
+}

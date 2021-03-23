3646c14a760a84deb7f6c80c5eb8c092c122fed1
ksualboy <danielmillanalvarez@gmail.com>
2021-03-23 12:37:49 +0000

    fixing header file and 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,12 @@ void free_listint2(listint_t **head)
 	listint_t *buscador = *head;
 	listint_t *temp;
 
-	if (!*head)
-		return;
-
 	while (buscador)
 	{
-		temp = buscador;
-		buscador = buscador->next;
-		free(temp);
+			temp = buscador;
+			buscador = buscador->next;
+			free(temp);
 	}
 	*head = NULL;
+	free(*head);
 }

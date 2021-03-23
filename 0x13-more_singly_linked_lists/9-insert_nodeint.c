21cf2c625011e517666bacf0a074c7de4111f056
ksualboy <danielmillanalvarez@gmail.com>
2021-03-23 14:13:16 +0000

    fixing NULL border case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,16 +12,17 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *this_node = *head;
+	listint_t *this_node;
 	listint_t *new;
 	unsigned int i;
 
-	if (!*head)
+	if (!head)
 		return (NULL);
 
 	if (idx == 0)
 		return (add_nodeint(head, n));
 
+	this_node = *head;
 	for (i = 1; i < idx; i++)
 	{
 		if (!this_node)

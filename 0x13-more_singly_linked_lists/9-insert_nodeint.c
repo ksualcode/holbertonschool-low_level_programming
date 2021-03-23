75950cab1a139ccb5d71ba5d8a3e9e2dbd8f5b17
ksualboy <danielmillanalvarez@gmail.com>
2021-03-23 13:40:21 +0000

    fixing border cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,9 +23,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		this_node = this_node->next;
 	}
 
-	if (!this_node)
-		return (NULL);
-
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);

8267fbad4fa646414e7f174781bc302e06881247
Ksualboy <77964799+Ksualboy@users.noreply.github.com>
2021-03-25 22:16:04 -0300

    Adding 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * set_bit - sets the value of a bit at a given index
+ * @n: number to process
+ * @index: index
+ *
+ * Return: 1 if sucess, -1 if fail
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int size, check;
+
+	size = sizeof(*n) * 8 - 1;
+	if (index > size)
+		return (-1);
+
+	check = 1 << index;
+	*n = mask | *n;
+
+	return (1);
+}

544601cd584f9fb12d12ed51c28c659969fd40c3
Daniel Millan <danielmillanalvarez@gmail.com>
2021-10-04 13:36:22 +0000

    adding readme, 0 and header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int linear_search(int *, size_t, int);
+
+
+#endif /* SEARCH_ALGOS_H */
